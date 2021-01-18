5f3f1746ab856903c22327d787e8d319aa8a85f5
zhengxuejiao <zhengxuejiao@autohome.com.cn>
2021-01-18 18:34:59 +0800

    feat: 新增 tapable.js

---
+++ packages/04.plugins/tapable.test.js
@@ -0,0 +1,46 @@
+const { SyncHook, SyncBailHook, AsyncParallelHook, AsyncSeriesHook } = require('tapable');
+
+class Lesson {
+  constructor() {
+    this.hooks = {
+      // 同步钩子，依次执行
+      // go: new SyncHook(['address']), // 都执行
+      go: new SyncBailHook(['address']), // 一旦有返回值，就退出
+      // 异步钩子，
+      // AsyncParallelHook 异步并行
+      leave: new AsyncParallelHook(['address']), // 遇到返回值，就不继续了
+    }
+  }
+
+  tap() {
+    // 往hooks容器中注册事件/添加函数
+    this.hooks.go.tap('class0318', (address) => {
+      console.log('class0318', address);
+
+      return 1;
+    })
+
+    this.hooks.go.tap('class0410', (address) => {
+      console.log('class0410', address);
+    });
+
+    this.hooks.leave.tapAsync('class0510', (name, age, cb) => {
+      setTimeout(() => {
+        console.log('class0510', name, age);
+        cb();
+      }, 1000);
+    })
+  }
+
+  start() {
+    // 触发hooks, call 全部触发
+    this.hooks.go.call('318');
+    this.hooks.leave.callAsync('jack', 18, function () {
+      console.log('所有leave容器中的函数触发完了，才触发，end~~')
+    });
+  }
+}
+
+const l = new Lesson();
+l.tap(); // 注册
+l.start(); // 启动
