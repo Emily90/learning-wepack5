134f7473c33c1a2bdd81a636a4c2cb6334a9c2d4
zhengxuejiao <zhengxuejiao@autohome.com.cn>
2021-01-19 10:25:36 +0800

    feat: 修改tapable.js

--- packages/04.plugins/tapable.test.js
+++ packages/04.plugins/tapable.test.js
@@ -8,34 +8,43 @@ class Lesson {
       go: new SyncBailHook(['address']), // 一旦有返回值，就退出
       // 异步钩子，
       // AsyncParallelHook 异步并行
-      leave: new AsyncParallelHook(['address']), // 遇到返回值，就不继续了
+      leave: new AsyncParallelHook(['name', 'age']), // 遇到返回值，就不继续了
     }
   }
 
   tap() {
     // 往hooks容器中注册事件/添加函数
     this.hooks.go.tap('class0318', (address) => {
-      console.log('class0318', address);
+      console.log('class0318 tap', address);
 
       return 1;
     })
 
     this.hooks.go.tap('class0410', (address) => {
-      console.log('class0410', address);
+      console.log('class0410 tap', address);
     });
 
     this.hooks.leave.tapAsync('class0510', (name, age, cb) => {
       setTimeout(() => {
-        console.log('class0510', name, age);
-        cb();
+        console.log('class0510 tapAsync', name, age, cb);
+        cb && cb();
       }, 1000);
+    });
+    
+    this.hooks.leave.tapPromise('class0610', (name, age) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          console.log('class0610 tapPromise', name, age);
+          resolve();
+        }, 1000);
+      })
     })
   }
 
   start() {
     // 触发hooks, call 全部触发
     this.hooks.go.call('318');
-    this.hooks.leave.callAsync('jack', 18, function () {
+    this.hooks.leave.callAsync('jack', 18, () => {
       console.log('所有leave容器中的函数触发完了，才触发，end~~')
     });
   }
